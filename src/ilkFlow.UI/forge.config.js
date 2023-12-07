6493f3a7ba9a6e40525dbbf2f07d9fc42474f4d6
Adem KOCAMAZ <kocamazadem@gmail.com>
2023-12-07 16:27:45 +0300

    Update

---
+++ src/ilkFlow.UI/forge.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    extraResource: [
+      '../ilkFlow.Backend/dist/manage/_internal',
+      '../ilkFlow.Backend/dist/manage/manage.exe',
+    ],
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
